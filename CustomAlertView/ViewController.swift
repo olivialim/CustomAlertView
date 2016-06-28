74ea0cb1caa25a929649f67a80b16a0d9ae79965
Olivia Lim <olivia.lim@rga.com>
2016-06-28 16:59:36 -0400

    Initial Commit

---
+++ CustomAlertView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomAlertView
+//
+//  Created by Olivia Lim on 6/28/16.
+//  Copyright © 2016 Olivia Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
